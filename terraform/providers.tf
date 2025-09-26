69f934050545728d3c12f7f9bebc5bb76aea4b36
marciomfilho <107187625+marciomfilho@users.noreply.github.com>
2025-09-26 11:53:45 -0300

    .

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,11 +1,5 @@
 provider "null" {}
 
-provider "ssh" {
-  host        = var.host
-  user        = var.user
-  private_key = file(var.private_key_path)
-}
-
 provider "kubernetes" {
   config_path = "~/.kube/config"
 }
